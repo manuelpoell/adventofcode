2488f41e02f289fad818268ef13101af198d5249
Manuel Poell <manuelpoell@gmx.at>
2024-09-26 21:17:07 +0200

    unit tests for ts main function

--- 2023/src/main.ts
+++ 2023/src/main.ts
@@ -1,5 +1,5 @@
 import { program } from 'commander';
-import * as fs from 'fs/promises';
+import fs from 'fs';
 import {
   Challenge,
   Day1,
@@ -31,101 +31,97 @@ import {
 
 export const main = () => {
   program
-    .option('-d, --day <dayNumber>', 'Specify number of day to solve', '1')
+    .option('-d, --day <dayNumber>', 'Specify number of day to solve', '0')
     .option('-f, --file <filepath>', 'Path to input file', './input.txt')
     .option('-p, --partTwo', 'Enable functions for part two', false)
     .parse();
   const { day, file, partTwo } = program.opts<{ day: string; file: string; partTwo: boolean }>();
 
-  fs.readFile(file, 'utf-8')
-    .then((content) => {
-      if (!content) {
-        console.log(`failed to read content of ${file}`);
-        return;
-      }
+  const content = fs.readFileSync(file, 'utf8');
+  if (!content) {
+    console.log(`failed to read content of ${file}`);
+    return;
+  }
 
-      let challenge: Challenge | null = null;
+  let challenge: Challenge | null = null;
+  switch (day) {
+    case '1':
+      challenge = new Day1(content, partTwo);
+      break;
+    case '2':
+      challenge = new Day2(content, partTwo);
+      break;
+    case '3':
+      challenge = new Day3(content, partTwo);
+      break;
+    case '4':
+      challenge = new Day4(content, partTwo);
+      break;
+    case '5':
+      challenge = new Day5(content, partTwo);
+      break;
+    case '6':
+      challenge = new Day6(content, partTwo);
+      break;
+    case '7':
+      challenge = new Day7(content, partTwo);
+      break;
+    case '8':
+      challenge = new Day8(content, partTwo);
+      break;
+    case '9':
+      challenge = new Day9(content, partTwo);
+      break;
+    case '10':
+      challenge = new Day10(content, partTwo);
+      break;
+    case '11':
+      challenge = new Day11(content, partTwo);
+      break;
+    case '12':
+      challenge = new Day12(content, partTwo);
+      break;
+    case '13':
+      challenge = new Day13(content, partTwo);
+      break;
+    case '14':
+      challenge = new Day14(content, partTwo);
+      break;
+    case '15':
+      challenge = new Day15(content, partTwo);
+      break;
+    case '16':
+      challenge = new Day16(content, partTwo);
+      break;
+    case '17':
+      challenge = new Day17(content, partTwo);
+      break;
+    case '18':
+      challenge = new Day18(content, partTwo);
+      break;
+    case '19':
+      challenge = new Day19(content, partTwo);
+      break;
+    case '20':
+      challenge = new Day20(content, partTwo);
+      break;
+    case '21':
+      challenge = new Day21(content, partTwo);
+      break;
+    case '22':
+      challenge = new Day22(content, partTwo);
+      break;
+    case '23':
+      challenge = new Day23(content, partTwo);
+      break;
+    case '24':
+      challenge = new Day24(content, partTwo);
+      break;
+    case '25':
+      challenge = new Day25(content, partTwo);
+      break;
+  }
 
-      switch (day) {
-        case '1':
-          challenge = new Day1(content, partTwo);
-          break;
-        case '2':
-          challenge = new Day2(content, partTwo);
-          break;
-        case '3':
-          challenge = new Day3(content, partTwo);
-          break;
-        case '4':
-          challenge = new Day4(content, partTwo);
-          break;
-        case '5':
-          challenge = new Day5(content, partTwo);
-          break;
-        case '6':
-          challenge = new Day6(content, partTwo);
-          break;
-        case '7':
-          challenge = new Day7(content, partTwo);
-          break;
-        case '8':
-          challenge = new Day8(content, partTwo);
-          break;
-        case '9':
-          challenge = new Day9(content, partTwo);
-          break;
-        case '10':
-          challenge = new Day10(content, partTwo);
-          break;
-        case '11':
-          challenge = new Day11(content, partTwo);
-          break;
-        case '12':
-          challenge = new Day12(content, partTwo);
-          break;
-        case '13':
-          challenge = new Day13(content, partTwo);
-          break;
-        case '14':
-          challenge = new Day14(content, partTwo);
-          break;
-        case '15':
-          challenge = new Day15(content, partTwo);
-          break;
-        case '16':
-          challenge = new Day16(content, partTwo);
-          break;
-        case '17':
-          challenge = new Day17(content, partTwo);
-          break;
-        case '18':
-          challenge = new Day18(content, partTwo);
-          break;
-        case '19':
-          challenge = new Day19(content, partTwo);
-          break;
-        case '20':
-          challenge = new Day20(content, partTwo);
-          break;
-        case '21':
-          challenge = new Day21(content, partTwo);
-          break;
-        case '22':
-          challenge = new Day22(content, partTwo);
-          break;
-        case '23':
-          challenge = new Day23(content, partTwo);
-          break;
-        case '24':
-          challenge = new Day24(content, partTwo);
-          break;
-        case '25':
-          challenge = new Day25(content, partTwo);
-          break;
-      }
-
-      console.log(challenge?.solve() ?? `Day ${day} not found`);
-    })
-    .catch((err) => console.log(`failed to parse file ${file}`, err));
+  console.log(challenge?.solve() ?? `Day ${day} not found`);
 };
 main();
